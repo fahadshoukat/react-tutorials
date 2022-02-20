cc05f8bc2e5f30a085ae92c85480485f13956520
fahadshoukat <fraza6970@gmail.com>
2022-02-20 22:49:56 +0500

    create hello component with jsx

--- react-tutorials/src/App.js
+++ react-tutorials/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Greet from "./components/Greet";
 import { Greet1 } from './components/Greet';
 import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div className="App">
       <h1>React Tutorials</h1>
-      <Greet />
+      {/* <Greet />
       <Greet1 />
-      <Welcome />
+      <Welcome /> */}
+      <Hello />
     </div>
   );
 }

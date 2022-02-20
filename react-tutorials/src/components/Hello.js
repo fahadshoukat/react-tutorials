1dac11adc9ae1375bcf578ab79f37b843840991a
fahadshoukat <fraza6970@gmail.com>
2022-02-20 22:51:58 +0500

    write Hello Component without JSX

--- react-tutorials/src/components/Hello.js
+++ react-tutorials/src/components/Hello.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Hello = () => {
-    return (
-        <div>
-            <h2>Hello Fahad</h2>
-        </div>
-    )
-}
+  // return (
+  //     <div>
+  //         <h2>Hello Fahad</h2>
+  //     </div>
+  // )
+
+    return React.createElement(
+    "div",
+    null,
+    React.createElement("h2", null, "Hello Fahad")
+  );
+};
 
 export default Hello;

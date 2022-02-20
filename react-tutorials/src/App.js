9665b1cb045cd4c501ae335d77867806d85cf0f9
fahadshoukat <fraza6970@gmail.com>
2022-02-20 22:24:16 +0500

    create component with ES6 arrow function

--- react-tutorials/src/App.js
+++ react-tutorials/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Greet from "./components/Greet";
+import { Greet1 } from './components/Greet';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Greet1 />
     </div>
   );
 }
--- react-tutorials/src/components/Greet.js
+++ react-tutorials/src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function Greet () {
-    return <h1>Hello Fahad this is a functional component.</h1>
+    return <h1>Hello Fahad this is a functional component with simple js function.</h1>
 }
+
+export const Greet1 = () => <h1>Hello Fahad this is a functional component with ES6 arrow function.</h1>
 
 export default Greet;

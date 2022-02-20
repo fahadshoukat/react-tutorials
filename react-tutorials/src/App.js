784ed9f99e5e4a2a73c7f945a224a78a7ca6712f
fahadshoukat <fraza6970@gmail.com>
2022-02-20 22:25:40 +0500

    add heading and change h1 to h2

--- react-tutorials/src/App.js
+++ react-tutorials/src/App.js
@@ -5,6 +5,7 @@ import { Greet1 } from './components/Greet';
 function App() {
   return (
     <div className="App">
+      <h1>React Tutorials</h1>
       <Greet />
       <Greet1 />
     </div>
--- react-tutorials/src/components/Greet.js
+++ react-tutorials/src/components/Greet.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Greet () {
-    return <h1>Hello Fahad this is a functional component with simple js function.</h1>
+    return <h2>Hello Fahad this is a functional component with simple js function.</h2>
 }
 
-export const Greet1 = () => <h1>Hello Fahad this is a functional component with ES6 arrow function.</h1>
+export const Greet1 = () => <h2>Hello Fahad this is a functional component with ES6 arrow function.</h2>
 
 export default Greet;

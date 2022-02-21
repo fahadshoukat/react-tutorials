851d08b37cd69644e1a15c4e7db77cf1fbc82d7a
fahadshoukat <fraza6970@gmail.com>
2022-02-21 22:29:44 +0500

    create counter and change value using setstate method

--- react-tutorials/src/App.js
+++ react-tutorials/src/App.js
@@ -4,12 +4,14 @@ import { Greet1 } from './components/Greet';
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <h1>React Tutorials</h1>
-      <Message />
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet />
       <Greet1 /> */}
       {/* <Welcome name="Sufyan" heroname="footballer">

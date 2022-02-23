78604382f6838693c531a9f3003421b965270e06
fahadshoukat <fraza6970@gmail.com>
2022-02-23 23:20:27 +0500

    conditionally render through element variable approach

--- react-tutorials/src/components/UserGreeting.js
+++ react-tutorials/src/components/UserGreeting.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserGreeting extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         isLoggedIn: true
-      }
-    }
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
 
   render() {
+    // if/else approach
 
-    // if/else approch 
+    // if(this.state.isLoggedIn){
+    //     return <div>Welcome Fahad</div>
+    // }else {
+    //     return <div>Welcome Guest</div>
+    // }
 
-    if(this.state.isLoggedIn){
-        return <div>Welcome Fahad</div>
-    }else {
-        return <div>Welcome Guest</div>
+    // element variable approach
+
+    let message;
+
+    if (this.state.isLoggedIn) {
+      return (message = <div>Welcome Fahad</div>);
+    } else {
+      return (message = <div>Welcome Guest</div>);
     }
+
     // return (
     //   <div>
     //       <div>Welcome Fahad</div>
@@ -28,4 +37,4 @@ class UserGreeting extends Component {
   }
 }
 
-export default UserGreeting
+export default UserGreeting;

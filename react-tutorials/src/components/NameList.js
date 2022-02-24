dd0c91e9149951742374bf2ee063d90861c65237
fahadshoukat <fraza6970@gmail.com>
2022-02-24 22:14:12 +0500

    add key attributes in person component

--- react-tutorials/src/components/NameList.js
+++ react-tutorials/src/components/NameList.js
@@ -5,33 +5,38 @@ function NameList() {
 
     const persons = [
         {
+            id: 1,
             name: 'Fahad',
             age: 23,
             skill: 'React JS'
         },
         {
+            id: 2,
             name: 'Zahid',
             age: 24,
             skill: 'WordPress'
         },
         {
+            id: 3,
             name: 'Umer',
             age: 25,
             skill: 'JavaScript'
         },
         {
+            id: 4,
             name: 'Bilal',
             age: 22,
             skill: 'SQL'
         },
         {
+            id: 5,
             name: 'Frasat',
             age: 23,
             skill: 'Oracle'
         }
     ]
 
-    const personList = persons.map(person => <Persons person={person} />)
+    const personList = persons.map(person => <Persons key={person.id} person={person} />)
     // const names = ['Fahad', 'Zahid', 'Umer', 'Sufyan', 'Ali']
 
     // const nameList = names.map(name => <h2>{name}</h2>)

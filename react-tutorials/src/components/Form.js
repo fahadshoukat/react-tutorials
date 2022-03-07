f6782a89f0b8873ce9d5a4f5dd303c47c8a85284
fahadshoukat <fraza6970@gmail.com>
2022-03-07 22:49:32 +0500

    destructure variables from state

--- react-tutorials/src/components/Form.js
+++ react-tutorials/src/components/Form.js
@@ -24,19 +24,20 @@ class Form extends Component {
     }
 
   render() {
+      const [userName, comments, topic] = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
           <div>
               <label>UserName</label>
-              <input type="text" value={this.state.userName} onChange={this.handleUserNameChange} />
+              <input type="text" value={userName} onChange={this.handleUserNameChange} />
           </div>
           <div>
               <label>comments</label>
-              <textarea value={this.state.comments} onChange={this.handleCommentsChange} />
+              <textarea value={comments} onChange={this.handleCommentsChange} />
           </div>
           <div>
               <label>Topic</label>
-              <select value={this.state.topic} onChange={this.handleTopicChange} >
+              <select value={topic} onChange={this.handleTopicChange} >
                   <option value="React">React</option>
                   <option value="Angular">Angular</option>
                   <option value="Veu">Veu</option>
